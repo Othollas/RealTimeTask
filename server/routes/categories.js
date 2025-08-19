ae49e646aecbc44eefbf160f9a914fc1d7534710
o_thollas <o_thollas@hotmail.com>
2025-08-19 09:24:25 +0200

    Modification de la route POST et des fichier du server

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -1,14 +1,16 @@
 import express from "express";
 import { getDB } from "../db.js";
 
-const router = express.Router();
 
-router.get("/", async (req, res)=>{
+const router = express.Router();
+;
+
+router.get("/", async (req, res) => {
     try {
         const categories = await getDB()
-        .collection('categories')
-        .find()
-        .toArray();
+            .collection('categories')
+            .find()
+            .toArray();
         res.json(categories);
     } catch (err) {
         console.error(err)
@@ -16,21 +18,34 @@ router.get("/", async (req, res)=>{
     }
 });
 
-router.post("/", async (req, res)=>{
+router.post("/", async (req, res) => {
     try {
-        const {name, description = null} = req.body; // récuperation du post
+        const { name, description, owner } = req.body; // récuperation du post
+        console.log(req.body);
+        
 
         // validation
-if(!name) {
-    return res.status(400).json({message: "Le nom est obligatoire"})
-}
+        if (!name) {
+            return res.status(400).json({ message: "Le nom est obligatoire" })
+        }
 
-// creation de la categorie
-const nouvelleCategorie = {name, category};
+        const newCategorie = {
+            name, 
+            description, 
+            owner: owner || null,
+            created_at: new Date(),
+            updated_at: new Date()
+        };
 
+        const result = await getDB().collection('categories').insertOne(newCategorie);
+   
+        
+        //Response avec succes
+       res.status(201).json(result);
 
     } catch (err) {
-        
+        //Gestion des erreurs 
+        res.status(500).json({ message: err.message })
     }
 })
 
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 
-import { connectDB } from "./db.js";
+import { connectDB, getDB } from "./db.js";
 import categoriesRouter from "./routes/categories.js";
 
 const app = express();
@@ -11,36 +11,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
 
 
-app.use("api/categories", categoriesRouter);
+app.use("/api/categories", categoriesRouter);
 
 const PORT = 3001;
 
-app.post("/contact", (req, res) => {
-    console.log(req.body);
-    const {name, email} = req.body;
-    console.log(`nouvel utilisateur ${name} qui à pour email ${email}`);
-    // res.json({ status: "Message reçu"})
-    res.redirect("http://localhost:5173")
-})
+// app.post("/contact", (req, res) => {
+//     console.log(req.body);
+//     const {name, email} = req.body;
+//     console.log(`nouvel utilisateur ${name} qui à pour email ${email}`);
+//     // res.json({ status: "Message reçu"})
+//     res.redirect("http://localhost:5173")
+// })
 
-app.listen(PORT, () => {
-        console.log(`Server is running on http://localhost:${PORT}`);
-    });
 
 connectDB().then(()=>{
     app.listen(PORT, () => {
         console.log(`Server is running on http://localhost:${PORT}`);
     });
 });
-
-// connexion MongoDB
-// mongoose.connect("mongodb://lcoalhost:27017/todo_realtime", {
-    
-//     useUnifiedTopology: true
-// }).then(()=> console.log('MongoDB connecté'))
-// .catch(err => console.error(err));
-
-//lancement du serveur HTTP
-// const server = app.listen(PORT, ()=>{
-//     console.log(`Serveur HTTP sur http:localhost:${PORT}`)
-// })
