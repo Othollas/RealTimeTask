1282162393bb9bc5dabf0202e9ab8f4c724f187f
o_thollas <o_thollas@hotmail.com>
2025-08-20 08:25:09 +0200

    Refactorisation pour crée function fetchCategorie pour hook

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,36 @@
 
 import { useState } from 'react';
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 import CategoryList from './components/CategoryList'
 import AddCategory from './components/CreateCategorie'
 
+
 function App() {
 
-   const [categories, setCategories] = useState([]);
-   const [error, setError] = useState(null);
+  const [categories, setCategories] = useState([]);
+  const [error, setError] = useState(null);
 
-const fetchCategorie = async () => {
-        fetch("http://localhost:3001/api/categories")
-            .then((res) => {
-                if (!res.ok) throw new Error("Erreur de chargement");
-                return res.json();
-            })
-            .then((data) => setCategories(data))
-            .catch((err) => setError(err.message))
-    }
-   
-    if (error) return <p>erreur : {error}</p>;
+  const fetchCategorie = async () => {
+    fetch("http://localhost:3001/api/categories")
+      .then((res) => {
+        if (!res.ok) throw new Error("Erreur de chargement");
+        return res.json();
+      })
+      .then((data) => setCategories(data))
+      .catch((err) => setError(err.message))
+  }
+
+  if (error) return <p>erreur : {error}</p>;
 
   return (
-  
+
     <div>
-      <CategoryList categories={categories}  fetchCategorie={fetchCategorie}/>
-      <AddCategory fetchCategorie={fetchCategorie}/>
-      </div>
-     
+      <CategoryList categories={categories} fetchCategorie={fetchCategorie} />
+      <AddCategory fetchCategorie={fetchCategorie} />
+    </div>
+
 
   )
 }
