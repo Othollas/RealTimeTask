bff076b900a377fbd8c15d03c48faf4df414cf69
o_thollas <o_thollas@hotmail.com>
2025-09-01 11:05:08 +0200

    Creation de la fonction login

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -5,9 +5,22 @@ export const register  =  async (userData) => {
     const response = await fetch(`${url}/register`, {
         method: "POST",
         headers:{"Content-Type": "application/json"},
-        body:JSON.stringify(userData)
+        body: JSON.stringify(userData)
     });
 
     
+    return response.json();
+}
+
+
+export const login = async (userData) => {
+    const response = await fetch(`${url}/login`, {
+        method: 'POST',
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(userData),
+        credentials: "include" 
+        
+    })
+
     return response.json();
 }
