e89cb44d34a28163b44c832bf22659e0ae59f0ac
o_thollas <o_thollas@hotmail.com>
2025-09-03 18:20:50 +0200

    Modification des routes pour prendre en compte le localstorage et la bdd

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import Task from '../schemas/taskSchema.js';
 import { ObjectId } from "mongodb";
-import verifyToken from '../function.js';
+import verifyToken, { generateId } from '../function.js';
 
 
 const router = express.Router();
@@ -11,12 +11,12 @@ const router = express.Router();
 router.get("/:id", verifyToken, async (req, res) => {
 
     try {
-        
+
         if (req.user) {
             const tasks = await Task.find({ category_id: req.params.id });
-            res.json({ tasks, source: "db" });
-        }else{
-            res.json({source: "Guest"})
+            res.json({ tasks: tasks, source: "db" });
+        } else {
+            res.json({ source: "Guest" })
         }
 
     } catch (err) {
@@ -25,28 +25,40 @@ router.get("/:id", verifyToken, async (req, res) => {
     }
 })
 
-router.post("/", async (req, res) => {
-
-
+router.post("/", verifyToken, async (req, res) => {
     try {
         const { title, description, id_category } = req.body;
-
 
         if (!title) {
             return res.status(400).json({ message: "Le nom est obligatoire" })
         }
 
-        const newTask = {
-            title: title,
-            description: description != '' ? description : null,
-            category_id: new ObjectId(id_category),
-            created_at: new Date(),
-            updated_at: new Date(),
+        if (req.user) {
+
+            const newTask = {
+                title: title,
+                description: description != '' ? description : null,
+                category_id: new ObjectId(id_category),
+                created_at: new Date(),
+                updated_at: new Date(),
+            }
+
+            const result = await Task.insertOne(newTask);
+            res.status(201).json({ result, source: "db" });
+        } else {
+            const newTask = {
+                _id: generateId(),
+                title: title,
+                description: description != '' ? description : null,
+                category_id: id_category,
+                created_at: new Date(),
+                updated_at: new Date(),
+                completed: false,
+                recovery_time: null,
+                point: null
+            }
+            res.json({ newTask, source: "Guest" });
         }
-
-        const result = await Task.insertOne(newTask)
-
-        res.status(201).json(result);
 
     } catch (err) {
         res.status(500).json({ message: err.message })
@@ -54,50 +66,80 @@ router.post("/", async (req, res) => {
 
 })
 
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", verifyToken, async (req, res) => {
     try {
-        if (!ObjectId.isValid(req.params.id)) {
-            return res.status(400).json({ message: 'ID invalide' });
-        };
-        const id = new ObjectId(req.params.id);
 
-        const result = await Task.deleteOne({ _id: id });
+        if (req.user) {
 
-        if (result.deletedCount === 0) {
-            return res.status(404).json({ message: 'Catégorie non trouvée' });
-        };
+            if (!ObjectId.isValid(req.params.id)) {
+                return res.status(400).json({ message: 'ID invalide' });
+            };
+            const id = new ObjectId(req.params.id);
 
-        res.json({
-            message: 'Tâche supprimée avec succès',
-            deletedCount: result.deletedCount
-        });
+            const result = await Task.deleteOne({ _id: id });
+
+            if (result.deletedCount === 0) {
+                return res.status(404).json({ message: 'Catégorie non trouvée' });
+            };
+
+            res.json({
+                message: 'Tâche supprimée avec succès',
+                deletedCount: result.deletedCount,
+                source: "db"
+            });
+        } else if (!req.user) {
+            const id = req.params.id
+            res.json({ id: id, source: "Guest" })
+        }
+
     } catch (error) {
         res.status(500).json({ message: err.message })
     };
 });
 
-router.put("/:id", async (req, res) => {
+router.put("/:id", verifyToken, async (req, res) => {
     try {
         const id = req.params.id;
-        const { title, description, created_at } = req.body;
+        const { title, description, created_at, category_id } = req.body;
 
-        const newData = {
-            title: title,
-            description: description == '' ? null : description,
-            created_at: new Date(created_at),
-            updated_at: new Date()
-        };
 
-        const result = await Task.updateOne({ _id: new ObjectId(id) }, newData);
+        if (req.user) {
+            const newData = {
+                title: title,
+                description: description == '' ? null : description,
+                created_at: new Date(created_at),
+                updated_at: new Date()
+            };
 
-        if (!result.acknowledged) {
-            return res.status(404).json({ message: "tache non trouvé" })
-        };
+            const result = await Task.updateOne({ _id: new ObjectId(id) }, newData);
 
-        res.json({
-            message: "Bravo tu as reussi",
-            modificatedCount: result.modifiedCount
-        });
+            if (!result.acknowledged) {
+                return res.status(404).json({ message: "tache non trouvé" })
+            };
+
+            res.json({
+                message: "Bravo tu as reussi",
+                modificatedCount: result.modifiedCount,
+                source: "db"
+            });
+        } else if (!req.user) {
+            const newData = {
+                _id: id,
+                title: title,
+                description: description == '' ? null : description,
+                category_id,
+                created_at: new Date(created_at),
+                updated_at: new Date(),
+                recovery_time: null,
+                point: null
+            };
+
+            res.json({
+                taskModify: newData,
+                source: "Guest"
+            })
+        }
+
 
     } catch (err) {
         res.status(500).json({ message: err.message })
