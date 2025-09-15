5d5cf57de6f59a29abc7916a5c960ae08dd1dbe1
o_thollas <o_thollas@hotmail.com>
2025-09-15 16:57:19 +0200

    Externalisation de la logique des EventBus

---
+++ client/src/service/bus.js
@@ -0,0 +1,20 @@
+const subscribers = {};
+
+export const EventBus = {
+    subscribe(event, callback) {
+        console.log(subscribers)
+        if(!subscribers[event]) subscribers[event] = [];
+        subscribers[event].push(callback);
+
+        // retroune une fonction pour se désabonner 
+        return () => {
+            subscribers[event] = subscribers[event].filter(cb => cb !== callback)
+        };
+    },
+
+    publish(event, data) {
+        if(!subscribers[event]) return;
+        subscribers[event].forEach(cb => cb(data));
+       
+    }
+};
