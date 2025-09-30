bef5a9f731a045d5c6538ff80f978ad31fb1264b
o_thollas <o_thollas@hotmail.com>
2025-09-30 11:40:52 +0200

    Ajout des commentaire pour la pages components MonCompte

--- client/src/pages/MonCompte.jsx
+++ client/src/pages/MonCompte.jsx
@@ -1,41 +1,81 @@
+/**
+ *  Composant MonCompte
+ * 
+ * Rôle : 
+ * Affiche les informations du groupe de l'utilisateur et la liste des membres.
+ * Redirige vers l'acceueil si l'utilisateur n'est pas connecté.
+ * 
+ * Entrées (props) :
+ * -user : Boolean indique si il y a un utilisateur (Affiner les details) 
+ * - loading : Boolean, indique si les infos utilisateur sont en cours de chargement.
+ * 
+ * Sorties / Effets : 
+ * -Navigue vers "/" (accueil) si l'utilisateur n'est pas connecté
+ * -Récupère via fetch les infos groupe et liste de membres
+ * - Met à jour l'etat local : group (nom du groupe), userGroup (liste de membre) 
+ */
+
+import { Error } from "mongoose";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const MonCompte = ({ user }) =>{
+const MonCompte = ({ user, loading }) => {
 
-const [group, setGroup] = useState(false);
-const [userGroup, setUserGroup] = useState([]); 
-const navigate = useNavigate();
+    // State local : titre du groupe de l'utilisateur 
+    const [group, setGroup] = useState(false);
 
-useEffect(()=>{
-    fetchGroup();
-}, [])
-
-const fetchGroup= () => {
-    // Fetch pour récuperer les groupe (ou les personnes dans le groupe pour les afficher )
+    // State local : Groupe de l'utilisateur 
+    const [userGroup, setUserGroup] = useState([]);
+    const navigate = useNavigate();
 
 
-    // si reponse alors json pour recuperer les données des users 
+console.log(userGroup) // ⚠️ log temporaire à enlever en prod
 
-    // si pas de personne catch error
-}
+// Redirige l'utilisateur vers l'acceuil s'il n'est pas connecté
+    useEffect(() => {
+        if (!user && !loading) {
+            navigate("/", { replace: true })
+        }
+    }, [loading]) // Dépend du chargement, pas besoin de rajouter user.
 
 
+    // au montage du composant, recuperation  des info du groupe
+    useEffect(() => {
+        fetchGroup();
+    }, [])
 
-if(user){
-    return(
+    // Fonction pour récupérer les données du groupe depuis l'API 
+    const fetchGroup = async () => {
+        // Fetch pour récuperer les groupe (ou les personnes dans le groupe pour les afficher )
+        fetch("http://localhost:3001/api/group", {
+            credentials:"include", // Pour inclure les cookies/Session
+            method: "GET"
+        })
+        .then((res)=>{
+            if(!res.ok){ throw new Error("erreur dans la demande du titre")}
+            return res.json()
+        })
+        .then(data=> {
+            // Met à jour l'état avec les données reçues
+            setGroup(data.titre_groupe); // Nom du groupe
+            setUserGroup(data.nom_groupe); // Liste des utilisateurs
+        })
+        .catch( err=> {
+            console.error("Erreur fetchGroup", err)
+        }) 
+    }
 
-    <div>
-        <h2>MonCompte</h2>
-        
-        <p>Personne dans le groupe : </p>
-    </div>
-)
-}else{
-    
-    {navigate("./moncompte")}
 
-}
+    return (
+
+        <div>
+            <h2>MonCompte</h2>
+            <p>Nom du groupe : {group ? group : null}</p>
+            <ul>Personne dans le groupe : {userGroup.map(user=> <li>{user.username}</li> )} </ul>
+            <button onClick={()=>navigate("/", {replace:true})}>Acceuil</button>
+        </div>
+    )
+
 
 };
 
